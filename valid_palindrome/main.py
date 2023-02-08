208731b0918bddff33e4010f526d386039d5a2e4
Dinay Sharipov <dinaysm@gmail.com>
2023-02-08 16:08:36 +0600

    feat: solution to valid palindrome w s: O(1)

--- valid_palindrome/main.py
+++ valid_palindrome/main.py
@@ -10,13 +10,30 @@ or false otherwise.
 '''
 
 def isPalindrome(s: str) -> bool:
-    # Two pointers from each end of the string
-    # T: O(s/2) S: O(1)
+    # # Two pointers from each end of a filtered string
+    # # T: O(s/2) S: O(1)
 
-    # Filter only alphabetic characters and make them lowercase
-    s = ''.join(filter(str.isalnum, s)).lower()
+    # # Filter only alphabetic characters and make them lowercase
+    # s = ''.join(filter(str.isalnum, s)).lower()
 
-    length = len(s)
-    for i in range(length // 2):
-        if s[i] != s[length - 1 - i]: return False
+    # length = len(s)
+    # for i in range(length // 2):
+    #     if s[i] != s[length - 1 - i]: return False
+    # return True
+
+
+
+    # Two pointers on the original string
+    
+    i = 0
+    j = len(s) - 1
+
+    while i < j:
+        while i < j and not s[i].isalnum():
+            i += 1
+        while i < j and not s[j].isalnum():
+            j -= 1
+        if s[i].lower() != s[j].lower(): return False
+        i += 1
+        j -= 1
     return True
