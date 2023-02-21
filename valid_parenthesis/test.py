11f7ec9b2b3a9a6a3839c76d4dcde5432f3bccda
Dinay Sharipov <dinaysm@gmail.com>
2023-02-21 18:08:56 +0600

    feat: solution for valid parenthesis

---
+++ valid_parenthesis/test.py
@@ -0,0 +1,20 @@
+from valid_parenthesis.main import isValid
+import unittest
+
+class Test_maxArea(unittest.TestCase):
+    def test_1(self):
+        s = "()"
+        self.assertEqual(isValid(s), True)
+    def test_2(self):
+        s = "()[]{}"
+        self.assertEqual(isValid(s), True)
+    def test_3(self):
+        s = "(]"
+        self.assertEqual(isValid(s), False)
+    def test_4(self):
+        s = "((((((])))))"
+        self.assertEqual(isValid(s), False)
+    
+
+if __name__ == '__main__':
+    unittest.main()
