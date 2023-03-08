f5c41c018ef69dfb8ba58d6e2dd427f2c63cc47b
Dinay Sharipov <dinaysm@gmail.com>
2023-03-08 11:15:32 +0600

    wip: reverse linked list

---
+++ reverse_linked_list/test.py
@@ -0,0 +1,10 @@
+from reverse_linked_list.main import Solution
+import unittest
+
+class Test_reversell(unittest.TestCase):
+    def test_1(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
